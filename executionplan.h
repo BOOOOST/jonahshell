72cb2ec88187426ede6c7e96327ac529fc62f50a
Isaac Salem <bigisaac.salem@gmail.com>
2021-10-09 02:01:34 -0600

    example convert vecto cmds to c string

--- executionplan.h
+++ executionplan.h
@@ -22,3 +22,29 @@ class ExecutionPlan {
         bool backGround;
 };
 #endif
+
+// psudo code 
+// assume the command read in from input is 'ls -l'
+// then vector<Command> commands woud contain:
+//           "ls" and "-l"
+// to convert to a c string (ie char *)
+//
+// loop over commands vector to count length of the 
+// command and option strings
+// int cmd_str_len = 0;
+// foreach (string cmdstr : command)
+// {   // note this does not account for spaces in between the options
+//     cmd_str_len += cmdstr.size();
+// }
+//
+// // convert to char *
+// char * cmd = new char[cmd_str_len];
+// char * cmd_pointer_copy = cmd;
+// strcpy ( argv_str, cmd_line.c_str() );
+// foreach (string cmdstr : command)
+// {   // note: again this does not account for spaces in between the options
+//     strcpy (cmd_pointer_copy, cmdstr.c_str() );
+//     cmd_pointer_copy += cmdstr.size();
+// }
+//
+//now cmd should be a pointer to a c string of the command to execute
